1fd3411bf3e262e865b0ec02ee97cf4d48db8fed
Konstantin Konovalov <ktwice@mail.ru>
2023-12-19 09:55:55 +0500

    Add files via upload

--- Bpdf.java
+++ Bpdf.java
@@ -4,7 +4,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.Optional;
 import java.util.regex.Pattern;
 import javax.imageio.ImageIO;
@@ -26,88 +25,70 @@ public class Bpdf {
         slice(new File(args[0]));
     }
     private static void slice(File f) {
-        if(f.isDirectory()) {
-            try { 
-                System.out.println(f.getPath());
-                Files.newDirectoryStream(f.toPath(), "*.pdf")
-                        .forEach((path)->pdfSlice(path.toFile()));
-                System.out.println("Processing of all pdf-files is completed.");
-            } catch (IOException e) {
-                System.out.println("IOException *** " + e.getMessage());
-            }
-        } else {
+        if(!f.isDirectory()) {
             pdfSlice(f);
+            return;
+        }
+        try { 
+            System.out.println(f.getPath());
+            Files.newDirectoryStream(f.toPath(), "*.pdf")
+                .forEach((path)->pdfSlice(path.toFile()));
+            System.out.println("Processing of all pdf-files is completed.");
+        } catch (IOException e) {
+            System.out.println("IOException *** " + e.getMessage());
         }
     }
     private static void pdfSlice(File f) {
         try {
             System.out.println(f.getPath());
             PDDocument pdd = Loader.loadPDF(f);
-            File dir = mkdirImages(f);
+String s = f.getName();
+File dir = new File(f.getParentFile(), s.substring(0, s.lastIndexOf('.')));
+dir.mkdir(); // create images-destination-dir
             int pcount = pdd.getNumberOfPages();
-            for (int pno=1; pno<=pcount; pno++) {
+            for (int pno=1; pno<=pcount; pno++) 
                 pageImageSlice(pdd, pno, dir);
-            }
             System.out.println("Processing of " + pcount + " pages is completed.");
         } catch (IOException e) {
             System.out.println("IOException *** " + e.getMessage());
         }
     }
-    private static File mkdirImages(File f) {
-        String fname = f.getName();
-        String dname = fname.substring(0, fname.lastIndexOf('.'));
-        File dir = new File(f.getParentFile(), dname);
-        dir.mkdir(); // create images-destination-dir
-        return dir;
-    }
-    private static String[] pageTextSlice(final String pageText) {
-        String[] orders = pOrderSeparator.split(pageText);
-        return Arrays.copyOf(orders, orders.length - 1);
-    }
-    private static String getPageText(PDDocument pdd, int pageno) throws IOException {
+    private static String pageText(PDDocument pdd, int pageno)
+            throws IOException {
         PDFTextStripper stripper = new PDFTextStripper();
         stripper.setStartPage(pageno);
         stripper.setEndPage(pageno);
         return stripper.getText(pdd);
     }
-    private static void pageImageSlice(PDDocument pdd
-            , int pno
-            , File dir) throws IOException {
-        String[] orderTexts = pageTextSlice(getPageText(pdd,pno));
-        if(orderTexts.length == 0) return;
-        System.out.println(" " + orderTexts.length + " orders on page# " + pno);
+    private static boolean pageImageSlice(PDDocument pdd
+            , int pno, File dir) throws IOException {
+        String[] orders = pOrderSeparator.split(pageText(pdd,pno));
+        if(orders.length < 2)
+            return true;
+        System.out.println(" " + (orders.length - 1) + " orders on page# " + pno);
         PDFRenderer r = new PDFRenderer(pdd);
         BufferedImage image = r.renderImage(pno - 1, IMAGE_SCALE);
-        switch(orderTexts.length) {
-            case 1:
-                ImageIO.write(image
-                    , "JPEG"
-                    , new File(dir, name(orderTexts[0]) + "p" + pno + ".jpg")
-                );
-                break;
-            case 2:
-                int h = image.getHeight();
-                int w = image.getWidth();
-                ImageIO.write(image.getSubimage(0, 0, w, h / 2)
-                    , "JPEG"
-                    , new File(dir, name(orderTexts[0]) + "t" + pno + ".jpg")
-                );
-                ImageIO.write(image.getSubimage(0, h / 2, w, h - h / 2)
-                    , "JPEG"
-                    , new File(dir, name(orderTexts[1]) + "b" + pno + ".jpg")
-                );
-                break;
-        }
+        if(orders.length == 2)
+            return ImageIO.write(image, "JPEG"
+                , new File(dir, name(orders[0]) + "p" + pno + ".jpg")
+            );
+        int h = image.getHeight();
+        int w = image.getWidth();
+        return ImageIO.write(image.getSubimage(0, 0, w, h / 2), "JPEG"
+            , new File(dir, name(orders[0]) + "t" + pno + ".jpg")
+        ) && ImageIO.write(image.getSubimage(0, h / 2, w, h - h / 2), "JPEG"
+            , new File(dir, name(orders[1]) + "b" + pno + ".jpg")
+        );
     }
     private static String name(final String orderText) {
+        final String token = "Плательщик";
         Optional<String> sline = pLineSeparator.splitAsStream(orderText)
-            .filter((line)->line.contains("Плательщик"))
+            .filter((line)->line.contains(token))
             .findFirst();
         if(!sline.isPresent()) return "";
         Optional<String> sname = pWordSeparator.splitAsStream(sline.get())
-            .filter(x->!x.isEmpty())
-            .reduce((r,x)->r + "-" + x);
-        if(!sname.isPresent()) return "";
-        return sname.get() + ".";
+            .filter(word->!word.isEmpty() && !word.contains(token))
+            .reduce((result,word)->result + "-" + word);
+        return !sname.isPresent() ? "" : sname.get() + ".";
     }
 }
